c77df8643f27c8dbd25fdf3b836feff5729dfa47
teofanaenachioiu <teofanaenachioiu@yahoo.com>
2020-03-06 14:42:53 +0200

    some changes

--- Tasks/src/main/java/tasks/services/TasksService.java
+++ Tasks/src/main/java/tasks/services/TasksService.java
@@ -10,7 +10,7 @@ import java.util.Date;
 
 public class TasksService {
 
-    private ArrayTaskList tasks;
+    private ArrliayTaskList tasks;
 
     public TasksService(ArrayTaskList tasks){
         this.tasks = tasks;
